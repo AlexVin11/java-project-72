d8202e3539506b32a2336199cb92bffba6b46841
Alexander Vinogradow <alukard981@gmail.com>
2024-09-18 19:39:03 +0300

    App class created, instanse of javalin created, loging config added in instanse. Javalin instanse could be started using start method

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,10 @@ package hexlet.code;
 import io.javalin.Javalin;
 
 public class App {
+    public static void main(String[] args) {
+        var app = getApp();
+        app.start();
+    }
     public static Javalin getApp() {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
