3d6400680d8c9ca03fe42076a6f80d0817f0635d
Alexander Vinogradow <alukard981@gmail.com>
2024-09-18 18:50:40 +0300

    javalin dependency added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.javalin:javalin:6.3.0")
 }
 
 tasks.test {
