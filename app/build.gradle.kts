12acc945c5e8e243d3b1b5891513eefa9c79ce56
Alexander Vinogradow <alukard981@gmail.com>
2024-09-18 19:12:01 +0300

    javalin rendering and slf4j (simple logging) dependencys added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("io.javalin:javalin:6.3.0")
+    implementation("io.javalin:javalin-rendering:6.3.0")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
+
 }
 
 tasks.test {
