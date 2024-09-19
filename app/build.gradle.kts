c294ab31ecf89c5694b111b13305daa8ce876dd8
Alexander Vinogradow <alukard981@gmail.com>
2024-09-19 21:44:46 +0300

    lombok, ben-manes, jte, hikariCP added, java plugin removed (i have application already)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,13 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
-    java
     checkstyle
     application
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    jacoco
+    id("io.freefair.lombok") version "8.6"
+    id("com.github.ben-manes.versions") version "0.51.0"
 }
 
 group = "hexlet.code"
@@ -22,9 +27,21 @@ dependencies {
     implementation("io.javalin:javalin:6.3.0")
     implementation("io.javalin:javalin-rendering:6.3.0")
     implementation("org.slf4j:slf4j-simple:2.0.16")
-
+    // https://mvnrepository.com/artifact/gg.jte/jte
+    implementation("gg.jte:jte:3.1.12")
+    // https://mvnrepository.com/artifact/com.zaxxer/HikariCP
+    implementation("com.zaxxer:HikariCP:5.1.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
