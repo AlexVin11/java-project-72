c11eaa75f379c3d4aaf914f9cd2e26fa019307b1
Alexander Vinogradow <alukard981@gmail.com>
2024-09-19 21:43:11 +0300

    step 2 basic app structure created, first handler "/" added

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,17 +1,27 @@
 package hexlet.code;
 
+import hexlet.code.util.NamedRoutes;
 import io.javalin.Javalin;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, SQLException {
         var app = getApp();
-        app.start();
+        app.get(NamedRoutes.rootPath(), ctx ->
+                ctx.result("Hello, world!"));
+        app.start(getPort());
     }
-    public static Javalin getApp() {
+    public static Javalin getApp() throws IOException, SQLException {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
         return app;
     }
 
+    public static int getPort() {
+        var port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
 }
