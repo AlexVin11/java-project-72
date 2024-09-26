f82fece53f4000cdbb4381b83efa5129d160b03e
Alexander Vinogradow <alukard981@gmail.com>
2024-09-26 19:44:39 +0300

    getDbConfig method added in App to receive a JDBC_DATABASE_URL from environment variable

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,22 +4,36 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import hexlet.code.util.NamedRoutes;
 import io.javalin.Javalin;
+import repository.BaseRepository;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.SQLException;
+import java.util.stream.Collectors;
 
 public class App {
+    private static final String LOCAL_H2_BASE = "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;";
+
     public static void main(String[] args) throws IOException, SQLException {
         var app = getApp();
         app.start(getPort());
     }
     public static Javalin getApp() throws IOException, SQLException {
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(getDbConfig());
+        var dataSource = new HikariDataSource(hikariConfig);
+        var url = App.class.getClassLoader().getResourceAsStream("schema.sql");
+        var sql = new BufferedReader(new InputStreamReader(url))
+                .lines().collect(Collectors.joining("\n"));
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+        BaseRepository.dataSource = dataSource;
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
-        var hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl("jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
-        var dataSource = new HikariDataSource(hikariConfig);
         app.get(NamedRoutes.rootPath(), ctx ->
                 ctx.result("Hello, world!"));
         return app;
@@ -28,5 +42,9 @@ public class App {
     public static int getPort() {
         var port = System.getenv().getOrDefault("PORT", "7070");
         return Integer.valueOf(port);
+    }
+
+    public static String getDbConfig() {
+        return System.getenv().getOrDefault("JDBC_DATABASE_URL", LOCAL_H2_BASE);
     }
 }
