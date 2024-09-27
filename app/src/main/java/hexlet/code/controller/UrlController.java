677458a1d05eb81b6ed435d5e71f648584e17853
Alexander Vinogradow <alukard981@gmail.com>
2024-09-28 00:46:10 +0300

    first method to UrlController added (to work with index.jte)

--- app/src/main/java/hexlet/code/controller/UrlController.java
+++ app/src/main/java/hexlet/code/controller/UrlController.java
@@ -1,4 +1,17 @@
 package hexlet.code.controller;
 
+import hexlet.code.dto.url.*;
+import io.javalin.http.Context;
+import repository.UrlRepository;
+
+import java.sql.SQLException;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+
 public class UrlController {
+    public static void index(Context ctx) throws SQLException {
+        var urls = UrlRepository.getEntities();
+        var page = new UrlsPage(urls);
+        ctx.render("index.jte", model("page", page));
+    }
 }
