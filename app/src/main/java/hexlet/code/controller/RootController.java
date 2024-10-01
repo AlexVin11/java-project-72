8eeece899602e50f927268bb0d7781d217b886fd
Alexander Vinogradow <alukard981@gmail.com>
2024-10-01 19:09:35 +0300

    RootController created and added- maybe i dont need it

---
+++ app/src/main/java/hexlet/code/controller/RootController.java
@@ -0,0 +1,12 @@
+package hexlet.code.controller;
+
+import io.javalin.http.Context;
+
+import java.sql.SQLException;
+
+public class RootController {
+
+    public static void index(Context ctx) throws SQLException {
+        ctx.render("index.jte");
+    }
+}
