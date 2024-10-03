da76338c657ccfa53e83f50b92a25432d1071626
Alexander Vinogradow <alukard981@gmail.com>
2024-10-03 17:17:39 +0300

    empty line removed in RootController class

--- app/src/main/java/hexlet/code/controller/RootController.java
+++ app/src/main/java/hexlet/code/controller/RootController.java
@@ -5,7 +5,6 @@ import io.javalin.http.Context;
 import java.sql.SQLException;
 
 public class RootController {
-
     public static void index(Context ctx) throws SQLException {
         ctx.render("index.jte");
     }
