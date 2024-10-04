20a626b19b588d8c301c94eb188d2745b10c421d
Alexander Vinogradow <alukard981@gmail.com>
2024-10-05 01:10:02 +0300

    urlPath method with String id parameter added in NamedRoutes class to operate with the page template to show the info about specific url by id

--- app/src/main/java/hexlet/code/util/NamedRoutes.java
+++ app/src/main/java/hexlet/code/util/NamedRoutes.java
@@ -7,6 +7,14 @@ public class NamedRoutes {
     }
 
     public static String urlsPath() {
-        return rootPath() + "urls";
+        return "/urls";
+    }
+
+    public static String urlPath(Long id) {
+        return urlPath(String.valueOf(id));
+    }
+
+    public static String urlPath(String id) {
+        return "/urls/" + id;
     }
 }
