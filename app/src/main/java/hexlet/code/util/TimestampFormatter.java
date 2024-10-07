edf3467a93b99c409c9ea056e7086ec659ce7049
Alexander Vinogradow <alukard981@gmail.com>
2024-10-08 01:24:51 +0300

    name of the final String variable changed to TIMESTAMP_FORMAT_PATTERN + made public (just in case- maybe i need it in testing)

--- app/src/main/java/hexlet/code/util/TimestampFormatter.java
+++ app/src/main/java/hexlet/code/util/TimestampFormatter.java
@@ -5,11 +5,11 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 
 public class TimestampFormatter {
-    private static final String CREATED_AT_FORMAT = "dd/MM/yyyy HH:mm";
+    public static final String TIMESTAMP_FORMAT_PATTERN = "dd/MM/yyyy HH:mm";
 
     public static String dateFormatter(Timestamp dateTime) {
         Date date = new Date(dateTime.getTime());
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(CREATED_AT_FORMAT);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(TIMESTAMP_FORMAT_PATTERN);
         return simpleDateFormat.format(date);
     }
 }
