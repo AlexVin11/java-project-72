2048b3f6e7abf1093b5aba0b38325214fa086b38
Alexander Vinogradow <alukard981@gmail.com>
2024-10-08 01:25:25 +0300

    Basic test created using mockito to test / and /urls index handlers

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,4 +1,38 @@
 package hexlet.code;
 
+
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import io.javalin.testtools.JavalinTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.mock;
+
 public class AppTest {
+    private final Context ctx = mock(Context.class);
+    private static Javalin app;
+
+    @BeforeEach
+    public void runApp() throws SQLException, IOException {
+       app = App.getApp();
+    }
+
+    @Test
+    public void indexRootHandlerReturnSuccess() {
+        JavalinTest.test(app, (server, client) -> {
+            assertThat(client.get("/").code()).isEqualTo(200);
+        });
+    }
+
+    @Test
+    public void indexUrlHandlerReturnSuccess() {
+        JavalinTest.test(app, (server, client) -> {
+            assertThat(client.get("/urls").code()).isEqualTo(200);
+        });
+    }
 }
