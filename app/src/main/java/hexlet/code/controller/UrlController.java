a5e027a731901d9f0b6dbfd4e797f1d2eb8874e8
Alexander Vinogradow <alukard981@gmail.com>
2024-10-16 19:47:22 +0300

    UrlController.java edited to work with flashType of flash message + do checkMethod created to make chacks. Seems to me that Object Mapper should be used differently here (error of the incorrect json shown if check initiated)

--- app/src/main/java/hexlet/code/controller/UrlController.java
+++ app/src/main/java/hexlet/code/controller/UrlController.java
@@ -3,9 +3,15 @@ package hexlet.code.controller;
 import hexlet.code.dto.urls.UrlPage;
 import hexlet.code.dto.urls.UrlsPage;
 import hexlet.code.model.Url;
+import hexlet.code.model.UrlCheck;
 import hexlet.code.util.NamedRoutes;
 import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import kong.unirest.UnirestException;
+import repository.UrlCheckRepository;
 import repository.UrlRepository;
 
 import java.net.MalformedURLException;
@@ -23,6 +29,8 @@ public class UrlController {
         var page = new UrlsPage(urls);
         String flash = ctx.consumeSessionAttribute("flash");
         page.setFlash(flash);
+        String flashType = ctx.consumeSessionAttribute("flashType");
+        page.setFlashType(flashType);
         ctx.render("urls/index.jte", model("page", page));
     }
 
@@ -48,13 +56,37 @@ public class UrlController {
             if (foundedUrl.isEmpty()) {
                 UrlRepository.save(url);
                 ctx.sessionAttribute("flash", "Страница успешно добавлена");
+                ctx.sessionAttribute("flashType", "alert-success");
             } else {
                 ctx.sessionAttribute("flash", "Страница уже существует");
+                ctx.sessionAttribute("flashType", "alert-info");
             }
             ctx.redirect(NamedRoutes.urlsPath());
         } catch (URISyntaxException | MalformedURLException | IllegalArgumentException e) {
             ctx.sessionAttribute("flash", "Некорректный URL");
+            ctx.sessionAttribute("flashType", "alert-danger");
             ctx.redirect(NamedRoutes.rootPath());
+        }
+    }
+
+    public static void doCheck(Context ctx) throws SQLException, UnirestException {
+        var urlId = ctx.pathParamAsClass("id", Long.class).get();
+        Url url = UrlRepository.findById(urlId)
+                .orElseThrow(() -> new NotFoundResponse("Url not found"));
+        try {
+            HttpResponse<JsonNode> jsonResponse = Unirest.get(url.getName()).asJson();
+            int statusCode = jsonResponse.getStatus();
+            String h1 = jsonResponse.getBody().getObject().getString("h1");
+            String title = jsonResponse.getBody().getObject().getString("title");
+            String description = jsonResponse.getBody().getObject().getString("description");
+            UrlCheck urlCheck = new UrlCheck();
+            urlCheck.setStatusCode(statusCode);
+            urlCheck.setH1(h1);
+            urlCheck.setTitle(title);
+            urlCheck.setDescription(description);
+            UrlCheckRepository.save(urlCheck, url);
+        } catch (UnirestException e) {
+            throw new UnirestException(e);
         }
     }
 }
