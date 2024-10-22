b12512a6fdb6f51fa241e17889afd03644879611
Alexander Vinogradow <alukard981@gmail.com>
2024-10-22 20:13:57 +0300

    Minor fixes for UrlController

--- app/src/main/java/hexlet/code/controller/UrlController.java
+++ app/src/main/java/hexlet/code/controller/UrlController.java
@@ -7,12 +7,6 @@ import hexlet.code.model.UrlCheck;
 import hexlet.code.util.NamedRoutes;
 import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
-import kong.unirest.HttpResponse;
-import kong.unirest.Unirest;
-import kong.unirest.UnirestException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import repository.UrlCheckRepository;
 import repository.UrlRepository;
 
@@ -21,14 +15,17 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class UrlController {
+
     public static void index(Context ctx) throws SQLException {
         var urls = UrlRepository.getEntities();
-        var page = new UrlsPage(urls);
+        List<UrlCheck> latestChecks = UrlCheckRepository.getLatestChecks();
+        var page = new UrlsPage(urls, latestChecks);
         String flash = ctx.consumeSessionAttribute("flash");
         page.setFlash(flash);
         String flashType = ctx.consumeSessionAttribute("flashType");
@@ -41,12 +38,14 @@ public class UrlController {
         var url = UrlRepository.findById(id)
                 .orElseThrow(() -> new NotFoundResponse("Url not found"));
         var page = new UrlPage(url);
+        String flash = ctx.consumeSessionAttribute("flash");
+        page.setFlash(flash);
+        String flashType = ctx.consumeSessionAttribute("flashType");
+        page.setFlashType(flashType);
         ctx.render("urls/show.jte", model("page", page));
     }
 
     public static void add(Context ctx) throws SQLException,
-            MalformedURLException,
-            URISyntaxException,
             IllegalArgumentException {
         var name = ctx.formParam("url");
         try {
@@ -68,35 +67,6 @@ public class UrlController {
             ctx.sessionAttribute("flash", "Некорректный URL");
             ctx.sessionAttribute("flashType", "alert-danger");
             ctx.redirect(NamedRoutes.rootPath());
-        }
-    }
-
-    public static void doCheck(Context ctx) throws SQLException, UnirestException {
-        var urlId = ctx.pathParamAsClass("id", Long.class).get();
-        Url url = UrlRepository.findById(urlId)
-                .orElseThrow(() -> new NotFoundResponse("Url not found"));
-        try {
-            HttpResponse<String> httpResponse = Unirest.get(url.getName()).asString();
-            if (httpResponse.getStatus() != 200) {
-                throw new UnirestException("Url: " + url.getName()
-                        + "return non 200 code. Status is: " + httpResponse.getStatus());
-            } else {
-                int statusCode = httpResponse.getStatus();
-                String responseBody = httpResponse.getBody();
-                Document document = Jsoup.parse(responseBody);
-                String title = document.title();
-                Element firstH1 = document.selectFirst("h1");
-                String h1 = (firstH1 != null) ? firstH1.text() : null;
-                String description = document.select("meta[name=description]").attr("content");
-                UrlCheck urlCheck = new UrlCheck();
-                urlCheck.setStatusCode(statusCode);
-                urlCheck.setH1(h1);
-                urlCheck.setTitle(title);
-                urlCheck.setDescription(description);
-                UrlCheckRepository.save(urlCheck, url);
-            }
-        } catch (UnirestException e) {
-            throw new UnirestException("Error during URL check: " + e.getMessage(), e);
         }
     }
 }
