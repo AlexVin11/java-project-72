e9595f418d485502601b45ab02bc2a5ab272252f
Alexander Vinogradow <alukard981@gmail.com>
2024-10-28 18:43:05 +0300

    Review 1 fixes. TimestampFormatter.java variable was renamed so it changed to all html templates

--- app/src/main/java/hexlet/code/util/TimestampFormatter.java
+++ app/src/main/java/hexlet/code/util/TimestampFormatter.java
@@ -4,7 +4,7 @@ import java.time.format.DateTimeFormatter;
 
 public class TimestampFormatter {
     public static final String TIMESTAMP_FORMAT_PATTERN = "dd/MM/yyyy HH:mm";
-    public static DateTimeFormatter formatter = formatter(TIMESTAMP_FORMAT_PATTERN);
+    public static DateTimeFormatter formatterType1 = formatter(TIMESTAMP_FORMAT_PATTERN);
 
     public static DateTimeFormatter formatter(String pattern) {
         return DateTimeFormatter.ofPattern(pattern);
