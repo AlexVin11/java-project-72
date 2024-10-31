d7bd99c792271cd94cc6de100bb6e1de85d98139
Alexander Vinogradow <alukard981@gmail.com>
2024-10-31 15:25:26 +0300

    Review 2 fixes. add method in UrlController.java i returned to its previous state. Main fixes was made in the UrlCheckController.java

--- app/src/main/java/hexlet/code/controller/UrlController.java
+++ app/src/main/java/hexlet/code/controller/UrlController.java
@@ -11,13 +11,11 @@ import hexlet.code.repository.UrlCheckRepository;
 import hexlet.code.repository.UrlRepository;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 import static io.javalin.rendering.template.TemplateUtil.model;
@@ -49,38 +47,22 @@ public class UrlController {
 
     public static void add(Context ctx) throws SQLException,
             IllegalArgumentException {
+        var name = ctx.formParam("url");
         try {
-            var name = ctx.formParam("url");
-            if (Objects.isNull(name)) {
-                ctx.sessionAttribute("flash", "Некорректный URL");
-                ctx.sessionAttribute("flashType", "alert-danger");
-                ctx.redirect(NamedRoutes.rootPath());
+            URL absoluteUrl = new URI(name).toURL();
+            String schema = absoluteUrl.toURI().getScheme();
+            String authority = absoluteUrl.toURI().getAuthority();
+            Url url = new Url(schema + "://" + authority);
+            Optional<Url> foundedUrl = UrlRepository.findByName(url.getName());
+            if (foundedUrl.isEmpty()) {
+                UrlRepository.save(url);
+                ctx.sessionAttribute("flash", "Страница успешно добавлена");
+                ctx.sessionAttribute("flashType", "alert-success");
             } else {
-                URL absoluteUrl = new URI(name).toURL();
-                HttpURLConnection huc = (HttpURLConnection) absoluteUrl.openConnection();
-                huc.setInstanceFollowRedirects(false);
-                huc.setRequestMethod("HEAD");
-                int responseCodeOfUrl = huc.getResponseCode();
-                if (responseCodeOfUrl != HttpURLConnection.HTTP_OK) {
-                    ctx.sessionAttribute("flash", "Некорректный URL");
-                    ctx.sessionAttribute("flashType", "alert-danger");
-                    ctx.redirect(NamedRoutes.rootPath());
-                } else {
-                    String schema = absoluteUrl.toURI().getScheme();
-                    String authority = absoluteUrl.toURI().getAuthority();
-                    Url url = new Url(schema + "://" + authority);
-                    Optional<Url> foundedUrl = UrlRepository.findByName(url.getName());
-                    if (foundedUrl.isEmpty()) {
-                        UrlRepository.save(url);
-                        ctx.sessionAttribute("flash", "Страница успешно добавлена");
-                        ctx.sessionAttribute("flashType", "alert-success");
-                    } else {
-                        ctx.sessionAttribute("flash", "Страница уже существует");
-                        ctx.sessionAttribute("flashType", "alert-info");
-                    }
-                    ctx.redirect(NamedRoutes.urlsPath());
-                }
+                ctx.sessionAttribute("flash", "Страница уже существует");
+                ctx.sessionAttribute("flashType", "alert-info");
             }
+            ctx.redirect(NamedRoutes.urlsPath());
         } catch (URISyntaxException | IllegalArgumentException | IOException e) {
             ctx.sessionAttribute("flash", "Некорректный URL");
             ctx.sessionAttribute("flashType", "alert-danger");
